d097ec044409180772ea2334288e3576c0eef765
Ryan McKeever <39560065+Rkfmckee@users.noreply.github.com>
2018-06-08 21:44:13 +0100

    08/06/2018

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private float speed = 5.0f;
+    private float jumpHeight = 10.0f;
+    private float gravityScale = 9.8f;
+    private float decelerateAmount;
+    private float xAxis;
+    private PlayerInput input;
+    private bool onGround = false;
+
+    private void Start()
+    {
+        input = gameObject.GetComponent<PlayerInput>();
+        //Default Movement Keys
+        input.setLeftKey(KeyCode.A);
+        input.setRightKey(KeyCode.D);
+        input.setJumpKey(KeyCode.Space);
+
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+    private void FixedUpdate () 
+    {
+        getMovement();
+
+    }
+
+    private void getMovement() 
+    {
+        xAxis = input.getXAxis();
+        if (!onGround) {
+            decelerateAmount = .02f;
+        } else {
+            decelerateAmount = .1f;
+        }
+
+        float offsetX = (gameObject.GetComponent<BoxCollider2D>().size.x * .55f);
+        float offsetY = (gameObject.GetComponent<BoxCollider2D>().size.y / 2);
+        float length = .05f;
+        RaycastHit2D hitTop = Physics2D.Raycast(new Vector2(transform.position.x + (xAxis * offsetX), transform.position.y + offsetY), new Vector2(xAxis, 0), length);
+        RaycastHit2D hitBottom = Physics2D.Raycast(new Vector2(transform.position.x + (xAxis * offsetX), transform.position.y - offsetY), new Vector2(xAxis, 0), length);
+
+        if (input.getLeftKey() || input.getRightKey()) 
+        {
+            if (hitTop || hitBottom)
+            {
+                Debug.DrawRay(new Vector2(transform.position.x+(xAxis*offsetX), transform.position.y + offsetY), new Vector2(xAxis*length, 0), Color.white, 1f, false);
+                Debug.DrawRay(new Vector2(transform.position.x+(xAxis*offsetX), transform.position.y - offsetY), new Vector2(xAxis*length, 0), Color.white, 1f, false);
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+            }
+            else 
+            {
+                //Use PlayerInput to move the player.
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xAxis * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+            }
+        } 
+        else 
+        {
+            //Gradually slow down
+            if (Mathf.Abs(gameObject.GetComponent<Rigidbody2D>().velocity.x) > 0) 
+            {
+                if (!hitTop && !hitBottom)
+                {
+                    gameObject.GetComponent<Rigidbody2D>().velocity -= new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x * decelerateAmount, 0);
+                }
+                else 
+                {
+                    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+                }
+            }
+        }
+
+        if (input.getJumpKey() && onGround) {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up*speed;
+            onGround = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Solid")
+        {
+            if (collision.contacts[0].normal.y > 0) 
+            {
+                onGround = true;
+            }
+        }
+    }
+
+    public bool isOnGround() 
+    {
+        return onGround;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    private KeyCode leftKey;
+    private KeyCode rightKey;
+    private KeyCode jumpKey;
+
+    private float xAxis;
+    private float yAxis;
+    private float deceleration;
+    private Player player;
+
+    private void Start()
+    {
+        player = gameObject.GetComponent<Player>();
+    }
+
+    public bool getLeftKey() 
+    {
+        return Input.GetKey(leftKey);
+    }
+
+    public bool getRightKey()
+    {
+        return Input.GetKey(rightKey); ;
+    }
+
+    public bool getJumpKey()
+    {
+        return Input.GetKeyDown(jumpKey);
+    }
+    
+    public float getXAxis() 
+    {
+        if (player.isOnGround()) 
+        {
+            deceleration = 3f;
+        } else {
+            deceleration = .5f;
+        }
+
+        if (getLeftKey())
+        {
+            xAxis -= 1f;
+        }
+        else if (getRightKey())
+        {
+            xAxis += 1f;
+        } 
+
+        xAxis = Mathf.Clamp(xAxis, -1, 1);
+        return xAxis;
+    }
+
+
+    public void setLeftKey(KeyCode key) {
+        leftKey = key;
+    }
+
+    public void setRightKey(KeyCode key)
+    {
+        rightKey = key;
+    }
+
+    public void setJumpKey(KeyCode key)
+    {
+        jumpKey = key;
+    }
+}
